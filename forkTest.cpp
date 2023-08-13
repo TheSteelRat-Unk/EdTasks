c919b34fdaff4c4812ef24ffaa057352cd163e6a
TheSteelRat-Unk <140090197+TheSteelRat-Unk@users.noreply.github.com>
2023-08-13 17:37:54 +0300

    Update forkTest.cpp

--- forkTest.cpp
+++ forkTest.cpp
@@ -4,12 +4,10 @@
 
 int main() {
     int pipefd[2];
+    int value = 42; 
   
     pid_t pid = fork();
-    if (pid == -1) {
-        std::cout << "fail" << std::endl;
-        return 1;
-    }
+   
 
     if (pid == 0) { 
         close(pipefd[1]); 
@@ -24,14 +22,13 @@ int main() {
         close(pipefd[0]); 
 
     } else { 
-        close(pipefd[0]); 
+        close(pipefd[0]);
 
-        int value = 42;
         int* address = &value;
 
         write(pipefd[1], &address, sizeof(address));
         std::cout << "Родительский процесс: Адрес переменной, отправленный в дочерний процесс: " << address << std::endl;
-        std::cout << "Родительский процесс: Значение переменной до : " << value << std::endl;
+        std::cout << "Родительский процесс: Значение переменной до : "                           << value   << std::endl;
 
         close(pipefd[1]); 
 
